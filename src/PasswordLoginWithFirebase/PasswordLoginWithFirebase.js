f32900e144742434b100d2df6dc3ef462fb1c70b
Samalu45 <cth7312@autuni.ac.nz>
2023-10-05 11:35:48 +1300

    nav bar working
    need to import other functions with the new layout

--- src/PasswordLoginWithFirebase/PasswordLoginWithFirebase.js
+++ src/PasswordLoginWithFirebase/PasswordLoginWithFirebase.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterAndLogin from "./RegisterAndLogin";
 import App from "../App";
 import ForgotPassword from "./ForgotPassword.js";
+import Project from "../Pages/Project"
+import Home from "../Pages/Home"
+import About from "../Pages/About"
+import SoundLibrary from "../SoundLibrary";
+import Track from "../Pages/Track";
 
 function PasswordLoginWithFirebase(){
     return(
@@ -10,6 +15,11 @@ function PasswordLoginWithFirebase(){
                 <Routes>
                     <Route path="/" element={<RegisterAndLogin/>} />
                     <Route path="/App" element={<App/>}/>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/Project" element={<Project />} />
+                    <Route path="/About" element={<About />} />
+                    <Route path="/SoundLibrary" element={<SoundLibrary />} /> 
+                    <Route path="/Track" element={<Track />} /> 
                     <Route path="/reset" element={<ForgotPassword/>} />
                 </Routes>
             </div>
